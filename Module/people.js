637a67170fa28aeaa753aa5cc5c40ae7771a326f
mh-saikat <mr.saikat3pp@gmail.com>
2025-04-26 20:55:26 +0600

    Module system done

---
+++ Module/people.js
@@ -0,0 +1,10 @@
+const people = ["Saikat", "Antor", "Robin"];
+
+function sayHello() {
+  console.log(`Hy my name is ${people[0]}`);
+}
+
+console.log(module);
+
+// module.exports = {people, sayHello};
+module.exports = people;
